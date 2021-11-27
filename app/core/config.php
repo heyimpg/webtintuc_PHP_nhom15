6093c507681bef1bd8c0dc481b180c7d7a4922c7
phuong.nguyen2 <tomodachy2000@gmail.com>
2021-11-27 15:51:13 +0700

    update mail template config

--- app/core/config.php
+++ app/core/config.php
@@ -3,5 +3,5 @@
     const DETAIL_URL = BASE_URL."detail/";
     const CATEGORY_URL = "http://localhost/CNPM/BTL/webtintuc_PHP_nhom15/?url=category/";
     const UPLOAD_FOLDER_PATH = "assets/img/bg-img/";
-    const EMAIL_UNAME = "tomodachy2000@gmail.com";
+    const EMAIL_UNAME = "abc@gmail.com";
 ?>
