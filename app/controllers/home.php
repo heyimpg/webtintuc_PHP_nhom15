9b1f8a9e2d4002e02c72197306562bad8208c09b
phuong.nguyen2 <tomodachy2000@gmail.com>
2021-11-27 15:48:08 +0700

    merge code 27/11

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -116,7 +116,42 @@
                 {
                     $to = EMAIL_UNAME;
                     $subject = "webtintuc - Thư liên hệ hợp tác";
-                    $message = "Tên: $name <br/>SĐT: $phone <br/>Email: $email";
+                    // $message = "Tên: <span style='color: red'>$name</span> <br/>SĐT: $phone <br/>Email: $email";
+                    $message = "<div class='main' 
+                            style='background-color: rgba(173, 139, 139, 0.2); text-align: center; padding: 10px'
+                        >
+                            <table
+                                style='border-spacing:0;border-collapse:collapse;padding:0;vertical-align:top;width:100%;'
+                                width='100%' valign='top'>
+                                <tbody>
+                                    <tr style='padding:0;vertical-align:top;text-align:left' valign='top' align='left'>
+                                        <td style='word-break:break-word;vertical-align:top;color:#172b4d;font-weight:normal;padding:0;margin:0;text-align:left;font-size:14px;line-height:19px;font-family:-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Noto Sans,Ubuntu,Droid Sans,Helvetica Neue,sans-serif;border-collapse:collapse'
+                                            valign='top' align='left'>
+                                            <div style='display:block;margin:0 auto;padding:12px 16px; text-align: center'>
+                                                <img height='50' width='100'
+                                                    src='https://cdn.icon-icons.com/icons2/1675/PNG/512/3890941-market-news-newspaper-stock_111181.png'
+                                                    alt='' style='display: unset; background-size: contain;'
+                                                    >
+                                                <h2 style='display: unset; font-family: monospace;'>Web tin tức</h2>
+                                            </div>
+            
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div style='border-spacing:0;border-collapse:collapse;padding:0;vertical-align:top;width:100%;'
+                            >
+                                <div class='box' style='width: 300px; height: 200px; background-color:white; margin: 0 auto; padding: 5px; text-align: left; border-radius: 5px;'>
+                                    <ul style='padding: 0'>
+                                        <li style='list-style: none; line-height: 30px; font-size: larger; text-align: center; font-family: cursive;'>Thông tin đối tác</li>
+                                        <li style='list-style: none; line-height: 30px; font-size: larger; font-family: -webkit-pictograph;'>Tên: $name</li>
+                                        <li style='list-style: none; line-height: 30px; font-size: larger; font-family: -webkit-pictograph;'>SĐT: $phone</li>
+                                        <li style='list-style: none; line-height: 30px; font-size: larger; font-family: -webkit-pictograph;'>Email: $email</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>";
+
                     $header = "From: abc@gmail.com \r\n";
                     $header .= "Content-type: text/html; charset=utf-8";
                     if(mail($to, $subject, $message, $header))
@@ -126,4 +161,3 @@
                 }
         }
     }
-?>
