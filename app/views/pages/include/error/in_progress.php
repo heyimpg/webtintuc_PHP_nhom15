87b5ea44a8619df3a6af25224105b253b683cc4a
Tran Hai <khoidaumoichotoi@gmail.com>
2021-11-30 01:12:34 +0700

    in progress

---
+++ app/views/pages/include/error/in_progress.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="<?= BASE_URL ?>">
+    <title>Từ chối truy cập </title>
+    
+    <link rel="shortcut icon" href="./assets/img/core-img/favicon.ico">
+    <!-- Bootstrap -->
+    <link href="assets/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="assets/admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="assets/admin/vendors/nprogress/nprogress.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="assets/admin/build/css/custom.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/admin/css/custom.css">
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <!-- page content -->
+            <div class="col-md-12">
+                <div class="col-middle">
+                    <div class="text-center">
+                        <img src="assets/admin/build/images/under-construction_geek_woman.png" alt="" class=" img-fluid rounded mx-auto d-block">
+                    </div>
+                    <div class="text-center text-center">
+                        <h2 class="error">Sản phẩm đang trong quá trình hoàn thiện</h2>
+                        <p class="error"><a class="error-link" href="javascript:window.history.back();">Quay về trang trước</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- /page content -->
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="assets/admin/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/admin/vendors/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- FastClick -->
+    <script src="assets/admin/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="assets/admin/vendors/nprogress/nprogress.js"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="assets/admin/build/js/custom.min.js"></script>
+</body>
+
+</html>
